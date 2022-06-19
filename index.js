bc46667ed2a0929d43adb47d447b29fd728e3122
SaharshPatel24 <srpatel56@myseneca.ca>
2022-06-19 04:23:54 -0400

    checkpoint

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+const expressip = require('express-ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
+app.use(expressip().getIpInfoMiddleware);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -22,10 +24,23 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function (req, res) {
+  var ip = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
 
+  console.log(ip);
+  var info = {
+    'ipaddress': ip,
+    'language': req.headers["accept-language"].split(',')[0],
+    'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+  };
+  res.send(info);
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
